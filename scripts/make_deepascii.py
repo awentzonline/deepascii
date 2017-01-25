1480dacba07de6676e6f7aef372f82386fd445f5
Adam Wentz <adam.wentz@dollarshaveclub.com>
2017-01-24 22:06:59 -0800

    default to average pooling since it seems to look nicer

--- scripts/make_deepascii.py
+++ scripts/make_deepascii.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     argparser.add_argument('--font-size', type=int, default=12)
     argparser.add_argument('--layer', default='block2_conv1')
     argparser.add_argument('--pool', type=int, default=2)
-    argparser.add_argument('--pool-type', default='max', help='"max" or "avg"')
+    argparser.add_argument('--pool-type', default='avg', help='"max" or "avg"')
     argparser.add_argument('--invert-color', action='store_true', help="Switch to black on white text")
     argparser.add_argument('--charset', default=None, help='Text characters to use')
     config = argparser.parse_args()
