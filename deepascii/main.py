0106cc3374aa29851c85381d845702aef362001f
Adam Wentz <adam.wentz@dollarshaveclub.com>
2017-01-27 19:07:03 -0800

    scale

--- deepascii/main.py
+++ deepascii/main.py
@@ -11,7 +11,11 @@ from .model import extract_charset_features, generate_charset, make_model
 
 def main(config):
     source_img = Image.open(config.source_glob)
-    source_img = source_img = source_img.convert('L').convert('RGB')
+    source_width, source_height = source_img.size
+    out_width = config.columns * scale_from_layer_name(config.layer) # chars
+    out_height = int(source_height / float(source_width) * out_width)
+    print('Resizing to {}'.format((out_width, out_height)))
+    source_img = source_img.convert('L').convert('RGB').resize((out_width, out_height), Image.BICUBIC)
     feature_axis = dict(tf=-1, th=1)[K.image_dim_ordering()]
     source_arr = img_to_array(source_img)
     print('Input image {} shape {}'.format(config.source_glob, source_arr.shape))
@@ -39,3 +43,12 @@ def main(config):
         for col in range(0, indexes.shape[1], 1):
             line += charset_chars[indexes[row, col]]
         print line
+
+
+def scale_from_layer_name(name):
+    scale_map = dict(block1=1, block2=2, block3=4, block4=8)
+    for prefix, scale in scale_map.items():
+        if name.startswith(prefix):
+            if 'pool' in name:
+                scale *= 2
+            return scale
